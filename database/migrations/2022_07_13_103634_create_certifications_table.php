5f9eb651b462ca25e62ac787a1d4e48512205c3a
Saransh Kumar <saranshkumar@Saransh-MBP.local>
2022-07-13 16:11:48 +0530

    Add in certification model and migration

---
+++ database/migrations/2022_07_13_103634_create_certifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCertificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('credential_id');
+            $table->string('url');
+            $table->date('issued_on');
+            $table->date('expired_on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certifications');
+    }
+}
