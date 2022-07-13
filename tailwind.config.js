5fb868ec1ed9fabb2a21ce82cf389d0bf3edd208
Saransh Kumar <saranshkumar@Saransh-MBP.local>
2022-07-13 15:12:11 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Lato", "Helvetica", "Arial", "sans-serif"],
+      mono: ["Roboto Mono"]
+    },    
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
+}
